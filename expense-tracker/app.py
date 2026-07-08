95a5de63a543dc45ecf9d1095533230f2e301eec
Vinit <vinitkumar25633@gmail.com>
2026-07-08 13:32:47 +0530

    app:route terms and condition

--- expense-tracker/app.py
+++ expense-tracker/app.py
@@ -50,6 +50,10 @@ def edit_expense(id):
 def delete_expense(id):
     return "Delete expense — coming in Step 9"
 
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
